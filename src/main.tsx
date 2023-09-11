221122c3a0d2b60eb6f186d27118a81c5511046f
Sofia Silva <sofiachaves.ps@outlook.com>
2023-09-11 21:28:26 +0100

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { Analytics } from '@vercel/analytics/react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App />
+        <Analytics />
+    </React.StrictMode>
+);
