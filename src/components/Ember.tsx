e5578cea96f868704df9211ffb5d21a935a0c841
Sofia Silva <sofiachaves.ps@outlook.com>
2023-09-13 13:13:18 +0100

    Metas

--- src/components/Ember.tsx
+++ src/components/Ember.tsx
@@ -1,6 +1,6 @@
 import ember from '../assets/ember.webp';
 
 const Ember = () => {
-    return <img src={ember} className="w-2.5" />;
+    return <img src={ember} className="w-2.5" alt="front ember symbol" />;
 };
 export default Ember;
--- src/components/Treasure.tsx
+++ src/components/Treasure.tsx
@@ -1,6 +1,6 @@
 import treasure from '../assets/coin.webp';
 
 const Treasure = () => {
-    return <img src={treasure} className="w-2.5" />;
+    return <img src={treasure} className="w-2.5" alt="treasure symbol" />;
 };
 export default Treasure;
